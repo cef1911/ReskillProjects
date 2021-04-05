d0f7486db12ec08f98ee1c4260de5203d27f2736
Chris Franklin <cef1911@hotmail.com>
2021-04-05 07:27:44 -0400

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,11 @@
+# Python For Loop
+# A for loop is used for iterating over a sequence.
+
+# Example
+# Print each fruit in a fruit list:
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+# The for loop does not require an indexing variable to set beforehand.
---
+++ loopingthroughastring.py
@@ -0,0 +1,8 @@
+# Looping Through a String
+
+# Even strings are iterable objects, they contain a sequence of characters:
+
+# Example
+
+for x in "banana":
+  print(x)
